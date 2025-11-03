cd47caf2eb83fccb27882299c6980fea9352b836
katie0109 <0109katie@naver.com>
2025-11-03 18:20:03 +0900

    feat: 보너스 번호 생성 기능

---
+++ src/test/kotlin/lotto/domain/BonusNumberTest.kt
@@ -0,0 +1,16 @@
+package lotto.domain
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.ValueSource
+
+class BonusNumberTest {
+
+    @ParameterizedTest
+    @ValueSource(ints = [1, 10, 20, 30, 45])
+    fun `1부터 45 사이의 숫자로 보너스 번호를 생성할 수 있다`(number: Int) {
+        val bonusNumber = BonusNumber(number)
+
+        assertThat(bonusNumber.value).isEqualTo(number)
+    }
+}
