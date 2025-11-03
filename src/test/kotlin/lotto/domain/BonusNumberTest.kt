09f7356a9dbde123e9b707aad1feb9227a770f2e
katie0109 <0109katie@naver.com>
2025-11-03 18:25:14 +0900

    feat: 보너스 번호 1~45 범위 검증 기능

--- src/test/kotlin/lotto/domain/BonusNumberTest.kt
+++ src/test/kotlin/lotto/domain/BonusNumberTest.kt
@@ -1,6 +1,7 @@
 package lotto.domain
 
 import org.assertj.core.api.Assertions.assertThat
+import org.assertj.core.api.Assertions.assertThatThrownBy
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.ValueSource
 
@@ -12,5 +13,13 @@ class BonusNumberTest {
         val bonusNumber = BonusNumber(number)
 
         assertThat(bonusNumber.value).isEqualTo(number)
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = [0, -1, 46, 100])
+    fun `보너스 번호가 1부터 45 범위를 벗어나면 예외가 발생한다`(number: Int) {
+        assertThatThrownBy { BonusNumber(number) }
+            .isInstanceOf(IllegalArgumentException::class.java)
+            .hasMessageContaining("[ERROR]")
     }
 }
