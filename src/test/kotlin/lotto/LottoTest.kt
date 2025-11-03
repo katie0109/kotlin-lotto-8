4df0f07f5b0f82073879a07a204983c52920874e
katie0109 <0109katie@naver.com>
2025-11-03 17:06:11 +0900

    fix: test 정리

--- src/test/kotlin/lotto/LottoTest.kt
+++ src/test/kotlin/lotto/LottoTest.kt
@@ -7,20 +7,20 @@ import org.junit.jupiter.api.Assertions.assertThrows
 import org.junit.jupiter.api.Assertions.assertTrue
 
 class LottoTest {
-//    @Test
-//    fun `로또 번호의 개수가 6개가 넘어가면 예외가 발생한다`() {
-//        assertThrows<IllegalArgumentException> {
-//            Lotto(listOf(1, 2, 3, 4, 5, 6, 7))
-//        }
-//    }
-//
-//    // TODO: 테스트가 통과하도록 프로덕션 코드 구현
-//    @Test
-//    fun `로또 번호에 중복된 숫자가 있으면 예외가 발생한다`() {
-//        assertThrows<IllegalArgumentException> {
-//            Lotto(listOf(1, 2, 3, 4, 5, 5))
-//        }
-//    }
+    @Test
+    fun `로또 번호의 개수가 6개가 넘어가면 예외가 발생한다`() {
+        assertThrows<IllegalArgumentException> {
+            Lotto(listOf(1, 2, 3, 4, 5, 6, 7))
+        }
+    }
+
+    // TODO: 테스트가 통과하도록 프로덕션 코드 구현
+    @Test
+    fun `로또 번호에 중복된 숫자가 있으면 예외가 발생한다`() {
+        assertThrows<IllegalArgumentException> {
+            Lotto(listOf(1, 2, 3, 4, 5, 5))
+        }
+    }
 
     // TODO: 추가 기능 구현에 따른 테스트 코드 작성
     @Test
@@ -29,26 +29,11 @@ class LottoTest {
 
         assertThat(lotto).isNotNull
     }
-    @Test
-    fun `로또 번호의 개수가 6개가 넘어가면 예외가 발생한다`() {
-        val exception = assertThrows(IllegalArgumentException::class.java) {
-            Lotto(listOf(1, 2, 3, 4, 5, 6, 7))
-        }
-        assertTrue(exception.message!!.contains("[ERROR]"))
-    }
 
     @Test
     fun `로또 번호의 개수가 6개 미만이면 예외가 발생한다`() {
         val exception = assertThrows(IllegalArgumentException::class.java) {
             Lotto(listOf(1, 2, 3, 4, 5))
-        }
-        assertTrue(exception.message!!.contains("[ERROR]"))
-    }
-
-    @Test
-    fun `로또 번호에 중복된 숫자가 있으면 예외가 발생한다`() {
-        val exception = assertThrows(IllegalArgumentException::class.java) {
-            Lotto(listOf(1, 2, 3, 4, 5, 5))
         }
         assertTrue(exception.message!!.contains("[ERROR]"))
     }
