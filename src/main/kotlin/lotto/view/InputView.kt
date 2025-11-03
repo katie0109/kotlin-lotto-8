b55a695b1239f1970c8a9d207dace55e08e6c011
katie0109 <0109katie@naver.com>
2025-11-03 19:54:52 +0900

    feat: 구매 금액 입력 재시도 기능

--- src/main/kotlin/lotto/view/InputView.kt
+++ src/main/kotlin/lotto/view/InputView.kt
@@ -1,11 +1,26 @@
 package lotto.view
 
 import camp.nextstep.edu.missionutils.Console
+import lotto.domain.PurchaseAmount
 
 object InputView {
 
     fun readPurchaseAmount(): String {
         println("구입금액을 입력해 주세요.")
         return Console.readLine()
+    }
+
+    fun readValidPurchaseAmount(): PurchaseAmount {
+        while (true) {
+            try {
+                val input = readPurchaseAmount()
+                val amount = input.toInt()
+                return PurchaseAmount(amount)
+            } catch (e: NumberFormatException) {
+                println("[ERROR] 구매 금액은 숫자여야 합니다.")
+            } catch (e: IllegalArgumentException) {
+                println(e.message)
+            }
+        }
     }
 }
