b7f65145351608d8e7cab2cdfa189c3053b121d6
katie0109 <0109katie@naver.com>
2025-11-03 18:26:54 +0900

    feat: 보너스 번호와 당첨 번호 중복 검증 기능

--- src/main/kotlin/lotto/domain/BonusNumber.kt
+++ src/main/kotlin/lotto/domain/BonusNumber.kt
@@ -1,16 +1,27 @@
 package lotto.domain
 
 @JvmInline
-value class BonusNumber(val value: Int) {
-
-    init {
-        require(value in MIN_LOTTO_NUMBER..MAX_LOTTO_NUMBER) {
-            "[ERROR] 보너스 번호는 $MIN_LOTTO_NUMBER 부터 $MAX_LOTTO_NUMBER 사이의 숫자여야 합니다."
-        }
-    }
+value class BonusNumber private constructor(val value: Int) {
 
     companion object {
         private const val MIN_LOTTO_NUMBER = 1
         private const val MAX_LOTTO_NUMBER = 45
+
+        operator fun invoke(value: Int): BonusNumber {
+            require(value in MIN_LOTTO_NUMBER..MAX_LOTTO_NUMBER) {
+                "[ERROR] 보너스 번호는 $MIN_LOTTO_NUMBER 부터 $MAX_LOTTO_NUMBER 사이의 숫자여야 합니다."
+            }
+            return BonusNumber(value)
+        }
+
+        fun of(value: Int, winningNumbers: WinningNumbers): BonusNumber {
+            require(value in MIN_LOTTO_NUMBER..MAX_LOTTO_NUMBER) {
+                "[ERROR] 보너스 번호는 $MIN_LOTTO_NUMBER 부터 $MAX_LOTTO_NUMBER 사이의 숫자여야 합니다."
+            }
+            require(value !in winningNumbers.getNumbers()) {
+                "[ERROR] 보너스 번호는 당첨 번호와 중복될 수 없습니다."
+            }
+            return BonusNumber(value)
+        }
     }
 }
--- src/test/kotlin/lotto/domain/BonusNumberTest.kt
+++ src/test/kotlin/lotto/domain/BonusNumberTest.kt
@@ -2,6 +2,7 @@ package lotto.domain
 
 import org.assertj.core.api.Assertions.assertThat
 import org.assertj.core.api.Assertions.assertThatThrownBy
+import org.junit.jupiter.api.Test
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.ValueSource
 
@@ -19,6 +20,27 @@ class BonusNumberTest {
     @ValueSource(ints = [0, -1, 46, 100])
     fun `보너스 번호가 1부터 45 범위를 벗어나면 예외가 발생한다`(number: Int) {
         assertThatThrownBy { BonusNumber(number) }
+            .isInstanceOf(IllegalArgumentException::class.java)
+            .hasMessageContaining("[ERROR]")
+    }
+
+    @Test
+    fun `보너스 번호가 당첨 번호와 중복되지 않으면 생성할 수 있다`() {
+        val winningNumbers = WinningNumbers.from("1,2,3,4,5,6")
+        val bonusNumber = BonusNumber.of(7, winningNumbers)
+
+        assertThat(bonusNumber.value).isEqualTo(7)
+    }
+
+    @Test
+    fun `보너스 번호가 당첨 번호와 중복되면 예외가 발생한다`() {
+        val winningNumbers = WinningNumbers.from("1,2,3,4,5,6")
+
+        assertThatThrownBy { BonusNumber.of(6, winningNumbers) }
+            .isInstanceOf(IllegalArgumentException::class.java)
+            .hasMessageContaining("[ERROR]")
+
+        assertThatThrownBy { BonusNumber.of(1, winningNumbers) }
             .isInstanceOf(IllegalArgumentException::class.java)
             .hasMessageContaining("[ERROR]")
     }
