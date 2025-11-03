4f248b711e881ebda49f9e22f87a1cd104110781
katie0109 <0109katie@naver.com>
2025-11-03 17:20:53 +0900

    feat: 구매 금액 1,000원 단위 검증 기능

--- src/main/kotlin/lotto/domain/PurchaseAmount.kt
+++ src/main/kotlin/lotto/domain/PurchaseAmount.kt
@@ -1,4 +1,15 @@
 package lotto.domain
 
 @JvmInline
-value class PurchaseAmount(val value: Int)
+value class PurchaseAmount(val value: Int) {
+
+    init {
+        require(value % LOTTO_PRICE == 0) {
+            "[ERROR] 구매 금액은 ${LOTTO_PRICE}원 단위여야 합니다."
+        }
+    }
+
+    companion object {
+        private const val LOTTO_PRICE = 1000
+    }
+}
--- src/test/kotlin/lotto/domain/PurchaseAmountTest.kt
+++ src/test/kotlin/lotto/domain/PurchaseAmountTest.kt
@@ -1,6 +1,7 @@
 package lotto.domain
 
 import org.assertj.core.api.Assertions.assertThat
+import org.assertj.core.api.Assertions.assertThatThrownBy
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.ValueSource
 
@@ -12,5 +13,13 @@ class PurchaseAmountTest {
         val purchaseAmount = PurchaseAmount(amount)
 
         assertThat(purchaseAmount.value).isEqualTo(amount)
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = [500, 1500, 2300, 10001])
+    fun `구매 금액이 1000원 단위가 아니면 예외가 발생한다`(amount: Int) {
+        assertThatThrownBy { PurchaseAmount(amount) }
+            .isInstanceOf(IllegalArgumentException::class.java)
+            .hasMessageContaining("[ERROR]")
     }
 }
